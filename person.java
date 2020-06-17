56b8e015b3192c77df9a727c5c5d53cf1f1e46ef
Lydia <acton.lydia@gmail.com>
2020-06-18 11:27:18 +1200

    Initial sharing of project

---
+++ person.java
@@ -0,0 +1,18 @@
+
+public class person
+{
+  private person follower;
+  private int id;
+  public person(int id){
+      this.id = id;
+  }
+  public void addfollower(person follower){
+      this.follower = follower;
+  }
+  public person follower(){
+      return follower;
+  }
+  public int myId(){
+      return id;
+  }
+}
---
+++ queue.java
@@ -0,0 +1,41 @@
+public class queue
+{
+    person first; //first person to join queue
+    person head; //person at front of queue
+    person tail; //person at end of queue
+    
+    public queue()
+    {
+        addFirstPerson(); //adds the first person, that people can follow
+        addQueuers(10); //add followers to first person
+        printQueue(first); //prints out the id's of people in queue
+        
+    }
+    
+    void addFirstPerson(){
+        first = new person(0); //creates new person with id 0
+        tail = first; //sets first in queue to them
+        head = first; //sets last in queue to them
+    }
+    
+    void addQueuers(int amount){
+        person queuer; //creates queuer variable
+        for(int i = 1; i<(amount+1); i++){
+            queuer = new person(i); //creates new queuer with id
+            tail.addfollower(queuer); //adds them to end of queue
+            tail = tail.follower(); //sets last in queue to them
+        }
+    }
+    
+    void leaveQueue(){
+        //head.follower() = head; //I need to create method to return follower
+    }
+    
+    void printQueue(person t){
+        System.out.println("The queue consists of:");
+        while (t != null){
+            System.out.println(t.myId());
+            t=t.follower();
+        }
+    }
+}
